50ef6c6aa041c7743525c77ef7e6ea5444959a6a
programmer-ejie <programmerejie@gmail.com>
2025-08-23 13:04:39 +0800

    changes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.manage');
 });
+Route::get('/admin/manage', function () {
+    return view('admin.manage');
+})->name('admin.manage');
